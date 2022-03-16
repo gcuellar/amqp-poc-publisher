a815f26c740ca759429649c4f5bd7151720e3da0
gcuellar <gcuellar@nosolosoftware.es>
2022-03-16 14:02:28 +0100

    Code improved

--- src/main/groovy/io/builders/amqp/poc/RabbitAmqpPocApplication.groovy
+++ src/main/groovy/io/builders/amqp/poc/RabbitAmqpPocApplication.groovy
@@ -1,7 +1,6 @@
 package io.builders.amqp.poc
 
 import io.builders.amqp.poc.producer.Publisher
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
